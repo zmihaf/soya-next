522c0d2fa5b2e0bb9ece3d391c38e4286678caf6
Sayuti Daniel <sayuti@traveloka.com>
2017-06-20 14:44:09 +0700

    Expose middleware

--- redux.js
+++ redux.js
@@ -1,2 +1,3 @@
 exports.applyReducers = require('./lib/redux/applyReducers').default;
 exports.storeEnhancer = require('./lib/redux/storeEnhancer').default;
+exports.middleware = require('./lib/redux/middleware').default;
