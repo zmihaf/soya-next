11e144897d8a5b922fe42d50631ec0c5e5d70e7b
Sayuti Daniel <sayuti@traveloka.com>
2017-07-11 19:20:24 +0700

    Add favicon and styles

--- packages/soya-next-cli/templates/pages/index.js
+++ packages/soya-next-cli/templates/pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
 import { createPage } from 'soya-next';
+import Layout from '../components/Layout/Layout';
 
 const IndexPage = () => (
-  <div>
+  <Layout>
     <Head><title>Soya Next Project</title></Head>
     <p>This is your project homepage</p>
-  </div>
+  </Layout>
 );
 
 export default createPage()(IndexPage);
