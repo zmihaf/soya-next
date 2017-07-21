73e8f7caf1edc70b904182dd52400ca72d87592f
Sayuti Daniel <sayuti@traveloka.com>
2017-07-21 17:01:14 +0700

    Add deprecation notice

---
+++ packages/soya-next/server/document.js
@@ -0,0 +1,5 @@
+throw new Error(
+  'createDocument is deprecated. ' +
+  'pages/_document.js is no longer needed to support server side CSS modules, it will be loaded automatically. ' +
+  'Please reinitialize your project using latest soya-next-cli to upgrade.'
+);
--- packages/soya-next/server/next-config.js
+++ packages/soya-next/server/next-config.js
@@ -1,1 +1,5 @@
-exports.createNextConfig = require('../lib/config/createNextConfig').default;
+throw new Error(
+  'createNextConfig is deprecated. ' +
+  'next.config.js is no longer needed to support soya-next features, it will be loaded automatically. ' +
+  'Please reinitialize your project using latest soya-next-cli to upgrade.'
+);
