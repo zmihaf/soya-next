af118fb19b8c41068f247b27c6b787a8106e38aa
Sayuti Daniel <sayuti@traveloka.com>
2017-07-21 12:36:43 +0700

    Wrap into describe scope

--- packages/soya-next/src/router/__tests__/createLocaleMiddleware.test.js
+++ packages/soya-next/src/router/__tests__/createLocaleMiddleware.test.js
@@ -1,55 +1,57 @@
 import createLocaleMiddleware from '../createLocaleMiddleware';
 
-it('requires default locale', () => {
-  expect(() => createLocaleMiddleware()).toThrowErrorMatchingSnapshot();
-});
+describe('Create Locale Middleware', () => {
+  it('requires default locale', () => {
+    expect(() => createLocaleMiddleware()).toThrowErrorMatchingSnapshot();
+  });
 
-it('requires site locales', () => {
-  expect(() => createLocaleMiddleware({ defaultLocale: 'id-id' })).toThrowErrorMatchingSnapshot();
-});
+  it('requires site locales', () => {
+    expect(() => createLocaleMiddleware({ defaultLocale: 'id-id' })).toThrowErrorMatchingSnapshot();
+  });
 
-let options, res, next;
-beforeEach(() => {
-  options = {
-    defaultLocale: 'id-id',
-    siteLocales: [
-      'id-id',
-      'en-id',
-      'en-sg',
-    ],
-  };
-  res = {};
-  next = jest.fn();
-});
+  let options, res, next;
+  beforeEach(() => {
+    options = {
+      defaultLocale: 'id-id',
+      siteLocales: [
+        'id-id',
+        'en-id',
+        'en-sg',
+      ],
+    };
+    res = {};
+    next = jest.fn();
+  });
 
-it('should fallback to default locale', () => {
-  const req = {
-    url: '/',
-  };
-  createLocaleMiddleware(options)(req, res, next);
-  expect(req).toMatchSnapshot();
-});
+  it('should fallback to default locale', () => {
+    const req = {
+      url: '/',
+    };
+    createLocaleMiddleware(options)(req, res, next);
+    expect(req).toMatchSnapshot();
+  });
 
-it('should fallback country to its default and remove locale segment from url', () => {
-  const req = {
-    url: '/en/',
-  };
-  createLocaleMiddleware(options)(req, res, next);
-  expect(req).toMatchSnapshot();
-});
+  it('should fallback country to its default and remove locale segment from url', () => {
+    const req = {
+      url: '/en/',
+    };
+    createLocaleMiddleware(options)(req, res, next);
+    expect(req).toMatchSnapshot();
+  });
 
-it('should match available site locales and remove locale segment from url', () => {
-  const req = {
-    url: '/en-sg/',
-  };
-  createLocaleMiddleware(options)(req, res, next);
-  expect(req).toMatchSnapshot();
-});
+  it('should match available site locales and remove locale segment from url', () => {
+    const req = {
+      url: '/en-sg/',
+    };
+    createLocaleMiddleware(options)(req, res, next);
+    expect(req).toMatchSnapshot();
+  });
 
-it('should fallback to default locale if none match', () => {
-  const req = {
-    url: '/ms-my/',
-  };
-  createLocaleMiddleware(options)(req, res, next);
-  expect(req).toMatchSnapshot();
+  it('should fallback to default locale if none match', () => {
+    const req = {
+      url: '/ms-my/',
+    };
+    createLocaleMiddleware(options)(req, res, next);
+    expect(req).toMatchSnapshot();
+  });
 });
