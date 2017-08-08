d6cc8df2d2f160b7278f7ee54ee863a55b8b961a
Sayuti Daniel <sayuti@traveloka.com>
2017-08-08 17:07:48 +0700

    Remove deprecated modules

--- packages/soya-next/server/next-config.js
+++
@@ -1,5 +0,0 @@
-throw new Error(
-  'createNextConfig is deprecated. ' +
-  'next.config.js is no longer needed to support soya-next features, it will be loaded automatically. ' +
-  'Please reinitialize your project using latest soya-next-cli to upgrade.'
-);
