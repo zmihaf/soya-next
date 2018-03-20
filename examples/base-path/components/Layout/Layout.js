77692d4c3682f082c7bdebe972856383c91ab85b
Sayuti Daniel <sayuti@traveloka.com>
2018-03-20 10:36:19 +0700

    Add with basePath example

---
+++ examples/base-path/components/Layout/Layout.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import logo from './logo.svg';
+import styles from './Layout.module.css';
+import '../../styles/global.css';
+
+const Layout = ({ children }) => (
+  <div className={styles.layout}>
+    <Head>
+      <link rel='shortcut icon' href='/static/favicon.ico' />
+    </Head>
+    <img src={logo} />
+    {children}
+  </div>
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
