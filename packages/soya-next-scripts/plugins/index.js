ac1f013a02e0195b9fde38954b75ca90d4eb5ddd
Sayuti Daniel <sayuti@traveloka.com>
2018-03-27 13:26:06 +0700

    Update plugins and next config

--- packages/soya-next-scripts/plugins/index.js
+++ packages/soya-next-scripts/plugins/index.js
@@ -1,3 +1,4 @@
+const config = require("config");
 const withAssetsImport = require("next-assets-import");
 const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 const withCSS = require("./withCSS");
@@ -6,19 +7,33 @@ const withConfig = require("next-config");
 const withDocument = require("./withDocument");
 const withExtractStyles = require("./withExtractStyles");
 const withMarlint = require("./withMarlint");
+const withResolver = require("./withResolver");
 const withSASS = require("./withSASS");
 const withSASSModules = require("./withSASSModules");
 const compose = require("lodash/flowRight");
 
-module.exports = compose(
-  withAssetsImport,
-  withBundleAnalyzer,
-  withDocument,
-  withExtractStyles,
-  withCSSModules,
-  withCSS,
-  withConfig,
-  withMarlint,
-  withSASSModules,
-  withSASS
-);
+module.exports = (nextConfig = {}) =>
+  compose(
+    withResolver,
+    withAssetsImport,
+    withBundleAnalyzer,
+    withDocument,
+    withExtractStyles,
+    withCSSModules,
+    withCSS,
+    withConfig,
+    withMarlint,
+    withSASSModules,
+    withSASS
+  )(
+    Object.assign(
+      {
+        assetPrefix: config.assetPrefix,
+        distDir: config.server.distDir,
+        generateEtags: config.server.generateEtags,
+        poweredByHeader: config.server.poweredByHeader,
+        useFileSystemPublicRoutes: config.server.useFileSystemPublicRoutes
+      },
+      nextConfig
+    )
+  );
